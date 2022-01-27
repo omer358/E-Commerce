9041b97a44c45621f98d3bda9f454d76acc2aeb4
omer358 <omermaki358@gmail.com>
2022-01-27 14:32:25 +0200

    initial static files

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import logging
+import os
 from pathlib import Path
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
